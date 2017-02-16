25fa5f69d38e22e49909c4047164afb7284b18e9
jpresnick <jpresnic@eckerd.edu>
2017-02-16 14:54:02 -0500

    check to make sure user is logged in before editing, posting, commenting

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ import time
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-								autoescape = True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+									autoescape=True)
+
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
+
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -33,22 +35,30 @@ class Handler(webapp2.RequestHandler):
 		uid = self.request.cookies.get('user')
 		self.user = uid and User.all().filter('user_id =', uid).get()
 
+
 # displays 10 most recent blog posts on the main page ('/')
 class MainPage(Handler):
 	def get(self):
 		entry = db.GqlQuery("SELECT * FROM Entry ORDER BY created DESC LIMIT 10")
 		home_status = 'class=active'
 		comments = db.GqlQuery("SELECT * FROM Comments ORDER BY created DESC")
-		self.render("blog.html", entry = entry, comments = comments, home_status = home_status, user_cookie = self.request.cookies.get('user'), like_unlike = "like")
-	
+		self.render("blog.html", entry=entry,
+								comments=comments,
+								home_status=home_status,
+								user_cookie=self.request.cookies.get('user'),
+								like_unlike="like")
+
 	def post(self):
 		entry = db.GqlQuery("SELECT * FROM Entry ORDER BY created DESC LIMIT 10")
 		home_status = 'class=active'
+		if not self.user:
+			return self.redirect('/login')
+
 		post_id = self.request.get('postID')
 		comment = self.request.get("comment")
 		clicked_like = self.request.get("clicked_like")
 		author = self.request.cookies.get('user')
-		post = Entry.get_by_id(int(post_id))	
+		post = Entry.get_by_id(int(post_id))
 		like_unlike = "like"
 		if not post:
 			self.error(404)
@@ -60,7 +70,7 @@ class MainPage(Handler):
 			for user in post.liked_by:
 				if user == self.user.key().id():
 					already_liked = True
-			if already_liked == True:
+			if already_liked:
 				post.likes = post.likes - 1
 				post.liked_by.remove(self.user.key().id())
 				post.put()
@@ -72,44 +82,49 @@ class MainPage(Handler):
 
 		# adds a new comment
 		if comment:
-			c = Comments(author = author, content = comment, post_id = int(post_id))
+			c = Comments(author=author, content=comment, post_id=int(post_id))
 			c.put()
 
 		# fetches existing comments
 		comments = db.GqlQuery("SELECT * FROM Comments ORDER BY created DESC")
 
 		time.sleep(0.2)
-		self.render("blog.html", entry = entry, comments = comments, home_status = home_status,
-								 user_cookie = self.request.cookies.get('user'), like_unlike = like_unlike)
-
+		self.render("blog.html", entry=entry, 
+								comments=comments, 
+								home_status=home_status,
+								user_cookie=self.request.cookies.get('user'), 
+								like_unlike=like_unlike)
 
 
 # creates a user database
 class User(db.Model):
-	user_id = db.StringProperty(required = True)
-	pw_hash = db.StringProperty(required = True)
+	user_id = db.StringProperty(required=True)
+	pw_hash = db.StringProperty(required=True)
 	email = db.StringProperty()
+
 
 # checks database to see if username exists
 def new_username(username):
-			u = User.all().filter('user_id =', username).get()
-			if u:
-				return False
-			else: 
-				return True
+	u = User.all().filter('user_id =', username).get()
+	if u:
+		return False
+	else: 
+		return True
+
 
 # checks to see if password is correct
 def valid_pw(name, pw, h):
-		    pw_hash, salt = h.split('|')
-		    if hashlib.sha256(name + pw + salt).hexdigest() == pw_hash:
-		        return True
+	pw_hash, salt = h.split('|')
+	if hashlib.sha256(name + pw + salt).hexdigest() == pw_hash:
+		return True
+
 
 # signup page allows users to create an account ('/signup')
 class Signup(Handler):
 	# gets user's registration criteria
 	def get(self):
 		signup_status = 'class=active'
-		self.render("signup.html", signup_status = signup_status)
+		self.render("signup.html", signup_status=signup_status)
 
 	def post(self):
 		# gets inputs and checks that they are valid
@@ -119,14 +134,15 @@ class Signup(Handler):
 		email = self.request.get("email")
 
 		USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+		PASSWORD_RE = re.compile(r"^.{3,20}$")
+		EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+
 		def valid_username(username):
 			return USER_RE.match(username)
 
-		PASSWORD_RE = re.compile(r"^.{3,20}$")
 		def valid_password(password):
 			return PASSWORD_RE.match(password)
 
-		EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 		def valid_email(email):
 			return EMAIL_RE.match(email)
 
@@ -134,9 +150,9 @@ class Signup(Handler):
 			return (''.join(random.choice(string.letters) for x in xrange(5)))
 
 		def make_pw_hash(name, pw):
-		    salt = make_salt()
-		    h = hashlib.sha256(name + pw + salt).hexdigest()
-		    return h + '|' + salt
+			salt = make_salt()
+			h = hashlib.sha256(name + pw + salt).hexdigest()
+			return h + '|' + salt
 
 		user_error = ""
 		user_exists_error = ""
@@ -161,24 +177,30 @@ class Signup(Handler):
 			if not valid_email(email):
 				email_error = "That's not a valid email."
 				error = True
-		if error == False:
+		if not error:
 			# add user to database
-			h = User(pw_hash = make_pw_hash(username, password), user_id = username, email = email)
+			h = User(pw_hash=make_pw_hash(username, password), 
+						user_id=username, email=email)
 			h.put()
 			# set user cookie
-			self.response.headers.add_header('Set-Cookie', 'user=%s; Path=/' % str(h.user_id))
+			self.response.headers.add_header('Set-Cookie', 
+												'user=%s; Path=/' % str(h.user_id))
 			time.sleep(0.2)
 			time.sleep(0.2)
 			self.redirect("/welcome")	
 		else:
-			self.render('signup.html', username = username, email = email, 
-									user_error = user_error, user_exists_error = user_exists_error, password_error = password_error, 
-									verify_error = verify_error, email_error = email_error)
+			self.render('signup.html', username=username, email=email, 
+										user_error=user_error, 
+										user_exists_error=user_exists_error, 
+										password_error=password_error, 
+										verify_error=verify_error, 
+										email_error=email_error)
+
 
 class Login(Handler):
 	def get(self):
 		login_status = 'class=active'
-		self.render("login.html", login_status = login_status)
+		self.render("login.html", login_status=login_status)
 
 	def post(self):
 		username = self.request.get("username")
@@ -199,49 +221,62 @@ class Login(Handler):
 			error = True
 
 		if error:
-			self.render('login.html', username = username, user_exists_error = user_exists_error, 
-									password_error = password_error)
+			self.render('login.html', username=username, 
+										user_exists_error=user_exists_error, 
+										password_error=password_error)
 		else:
-			#set user cookie and redirect to welcome page
-			self.response.headers.add_header('Set-Cookie', 'user=%s; Path=/' % str(u.user_id))
+			# set user cookie and redirect to welcome page
+			self.response.headers.add_header('Set-Cookie', 
+								'user=%s; Path=/' % str(u.user_id))
 			self.redirect("/welcome")
+
 
 class Logout(Handler):
 	def get(self):
 		# clears cookie
 		self.response.headers.add_header('Set-Cookie', 'user=%s; Path=/' % "")	
-		self.redirect("/signup")
+		self.redirect("/login")
+
 
 # user is redirected here after login or signup
 class WelcomeHandler(Handler):
 	def get(self):
 		user_info = self.request.cookies.get('user')	
-		self.render("welcome.html", username = user_info)
+		self.render("welcome.html", username=user_info)
+
 
 # table of blog posts
 class Entry(db.Model):
-	subject = db.StringProperty(required = True)
-	content = db.TextProperty(required = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-	last_modified = db.DateTimeProperty(auto_now = True)
-	author = db.StringProperty(required = True)
+	subject = db.StringProperty(required=True)
+	content = db.TextProperty(required=True)
+	created = db.DateTimeProperty(auto_now_add=True)
+	last_modified = db.DateTimeProperty(auto_now=True)
+	author = db.StringProperty(required=True)
 	likes = db.IntegerProperty(default=0)
 	liked_by = db.ListProperty(int)
 
 	def render(self, cookie=None, comments="", like_unlike=""):
 		self._render_text = self.content.replace('\n', '<br>')
-		return render_str("post.html", p = self, user_cookie = cookie, comments = comments, like_unlike = like_unlike)
- # table of comments
+		return render_str("post.html", p=self, 
+										user_cookie=cookie, 
+										comments=comments, 
+										like_unlike=like_unlike)
+
+
+# table of comments
 class Comments(db.Model):
-	author = db.StringProperty(required = True)
-	content = db.TextProperty(required = True)
-	post_id = db.IntegerProperty(required = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-	last_modified = db.DateTimeProperty(auto_now = True)
+	author = db.StringProperty(required=True)
+	content = db.TextProperty(required=True)
+	post_id = db.IntegerProperty(required=True)
+	created = db.DateTimeProperty(auto_now_add=True)
+	last_modified = db.DateTimeProperty(auto_now=True)
 
 	def render(self, cookie=None, post_id=""):
 		self._render_text = self.content.replace('\n', '<br>')
-		return render_str("comment.html", c = self, user_cookie = cookie, post_id = post_id)
+		return render_str("comment.html", c=self, 
+											user_cookie=cookie, 
+											post_id=post_id)
+
 
 # page to display an individual blog post
 class PermalinkHandler(Handler):
@@ -252,26 +287,35 @@ class PermalinkHandler(Handler):
 			self.error(404)
 			return
 		
-		self.render("permalink.html", post = post, user_cookie = self.request.cookies.get('user'))
+		self.render("permalink.html", post=post, 
+										user_cookie=self.request.cookies.get('user'))
+
 
 # allows user to submit a new post if they are logged in
 class NewPostHandler(Handler):
 	def get(self):
 		newpost_status = 'class=active'
-		self.render("newpost.html", newpost_status = newpost_status)
+		self.render("newpost.html", newpost_status=newpost_status)
+
 	def post(self):
+		if not self.user:
+			return self.redirect('/login')
+
 		subject = self.request.get("subject")
 		content = self.request.get("content")
 		author = self.request.cookies.get('user')
 
 		if subject and content:
-			e = Entry(subject = subject, content = content, author = author)
+			e = Entry(subject=subject, content=content, author=author)
 			e.put()
 			self.redirect('/' + str(e.key().id()))
 
 		else:
 			error = "You must submit a subject and some content."
-			self.render("newpost.html", subject = subject, content = content, error = error)
+			self.render("newpost.html", subject=subject, 
+											content=content, 
+											error=error)
+
 
 # allows user to edit and delete their own posts
 class EditPost(Handler):
@@ -283,77 +327,110 @@ class EditPost(Handler):
 			self.error(404)
 			return
 
-		self.render("editpost.html", subject = post.subject, content = post.content)
+		if self.request.cookies.get('user') == post.author:
+			self.render("editpost.html", subject=post.subject, content=post.content)
+		else:
+			self.redirect('/editerror')
 
 	def post(self):
+		if not self.user:
+			return self.redirect('/login')
+
 		subject = self.request.get("subject")
 		content = self.request.get("content")
 		post_id = self.request.get("id")
 		key = db.Key.from_path("Entry", int(post_id))
 		post = db.get(key)
+		if not post:
+			self.error(404)
+			return
 
-		delete = self.request.get("delete")
-		cancel = self.request.get("cancel")
-		if delete:
-			post.delete()
-			self.redirect('/confirm-delete')
+		if self.request.cookies.get('user') == post.author:
+			delete = self.request.get("delete")
+			cancel = self.request.get("cancel")
+			if delete:
+				post.delete()
+				self.render('/confirm-delete')
 
-		elif cancel:
-			self.redirect('/')
+			elif cancel:
+				self.redirect('/')
 
-		elif subject and content:
-			post.subject = subject
-			post.content = content
-			post.put()
-			self.redirect('/' + str(post.key().id()))
+			elif subject and content:
+				post.subject = subject
+				post.content = content
+				post.put()
+				self.redirect('/' + str(post.key().id()))
 
-		else: 
-			error = "You must submit a subject and some content."
-			self.render("editpost.html", subject = subject, content = content, error = error)
+			else: 
+				error = "You must submit a subject and some content."
+				self.render("editpost.html", subject=subject, content=content, error=error)
+		else:
+			self.redirect("/editerror")
+
 
 # this page allows a user to edit or delete one of their comments
 class EditCommentHandler(Handler):
 	def get(self):
+		if not self.user:
+			return self.redirect('/login')
+
 		comment_id = self.request.get("id")
 		key = db.Key.from_path("Comments", int(comment_id))
 		comment = db.get(key)
+
 		if not comment:
 			self.error(404)
 			return
 
-		self.render("edit-comment.html", content = comment.content)
+		if self.request.cookies.get('user') == comment.author:
+			self.render("edit-comment.html", content=comment.content)
+		else:
+			self.redirect('/editerror')
 
 	def post(self):
+		if not self.user:
+			return self.redirect('/login')
+
 		content = self.request.get('content')
 		comment_id = self.request.get("id")
 		key = db.Key.from_path("Comments", int(comment_id))
+		comment = db.get(key)
 
-		delete = self.request.get("delete")
-		cancel = self.request.get("cancel")
-		if delete:
-			comment = db.get(key)
-			comment.delete()
-			self.redirect('/confirm-delete/?id=comment')
+		if self.request.cookies.get('user') == comment.author:
+			delete = self.request.get("delete")
+			cancel = self.request.get("cancel")
+			if delete:
+				comment.delete()
+				self.redirect('/confirm-delete/?id=comment')
 
-		elif cancel:
-			self.redirect('/')
+			elif cancel:
+				self.redirect('/')
 
-		elif content:
-			comment = db.get(key)
-			comment.content = content
-			comment.put()
-			time.sleep(0.2)
-			self.redirect('/')
-		
-		else: 
-			error = "Your comment is blank."
-			self.render("edit-comment.html", content = content, error = error)
+			elif content:
+				comment.content = content
+				comment.put()
+				time.sleep(0.2)
+				self.redirect('/')
+			
+			else: 
+				error = "Your comment is blank."
+				self.render("edit-comment.html", 
+							content=content, 
+							error=error)
+		else:
+			self.redirect('/editerror')
+
 
 # user redirected here if they successfully delete one of their posts or comments
 class ConfirmDeleteHandler(Handler):
 	def get(self):
 		delete_type = self.request.get('id')
-		self.render("confirm-delete.html", type = delete_type)
+		self.render("confirm-delete.html", type=delete_type)
+
+
+class EditErrorHandler(Handler):
+	def get(self):
+		self.render("editerror.html")
 
 app = webapp2.WSGIApplication([('/', MainPage),
 								("/welcome", WelcomeHandler),
@@ -366,5 +443,6 @@ app = webapp2.WSGIApplication([('/', MainPage),
 								('/editpost/?', EditPost),
 								('/confirm-delete/?', ConfirmDeleteHandler),
 								('/edit-comment/?', EditCommentHandler),
+								('/editerror', EditErrorHandler),
 								],
     							debug=True)
